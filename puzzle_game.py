d7e54f6a1b4d56aa3824396132700e8db2edc2f8
blokkies48 <francisjlloyd15@gmail.com>
2022-09-02 18:27:43 +0200

    update to puzzle_game

--- puzzle_game.py
+++ puzzle_game.py
@@ -5,26 +5,23 @@ import os
 shutil.unpack_archive("unzip_me_for_instructions.zip","unzipped_puzzle_game","zip")
 
 import re
-def check_num(file):
-    return re.findall(r"(\d{3})-(\d{3})-(\d{4})",file)
+def check_num(str1):
+    return re.search(r"(\d{3})-(\d{3})-(\d{4})",str1)
 
 mydict = {1:"One", 2: "Two", 3:"Three",4:"Four",5:"Five"}
 n = 1
 
 while True:
-    file_path = f"F:\\Pyphon practice\\unzipped_puzzle_game\\extracted_content\\{mydict[n]}"
+    file_path = f"{os.getcwd()}\\unzipped_puzzle_game\\extracted_content\\{mydict[n]}"
     for folder, sub_folder, files in os.walk(file_path):
         
         for file in files:
-            f = open(file_path + "\\" + file,'r')
-            for word in f:
-                cell_num = check_num(word)
-            f.close()
-            if len(cell_num) > 0:
-                cell_num_str = " "
-                for i in cell_num:
-                    print(cell_num_str.join(i))
-                
+            #f = open(file_path + "\\" + file,'r')
+            with open(file_path + "\\" + file) as f:
+                for word in f:
+                    cell_num = check_num(word)
+                if cell_num != None:
+                    print(cell_num.group())            
     n+=1
     if n == 6:
         break   
