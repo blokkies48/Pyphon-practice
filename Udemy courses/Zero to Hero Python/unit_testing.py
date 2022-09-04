1485ef6e4df388d7b94a36bafde2fd838c35189b
blokkies48 <francisjlloyd15@gmail.com>
2022-09-04 14:20:35 +0200

    Did a bit of orginizing in udemy courses

---
+++ Udemy courses/Zero to Hero Python/unit_testing.py
@@ -0,0 +1,21 @@
+'''
+A very simple script
+'''
+
+
+#get the min num methods
+from operator import index
+from traceback import print_tb
+
+
+def min_num(list1):
+    return min(list1)#no need to set a variable
+
+
+#random list i made
+list_of_nums = [3,46,78,9,2]
+
+
+#Calling the list func and passing the random list to it
+min_num(list_of_nums)
+
